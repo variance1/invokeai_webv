70e6387bffcb319add247c4e21db561ba4df5429
variance <variance45@gmail.com>
2022-10-07 00:22:23 +0000

    InvokeAI basic implementation in FastAPI

--- app.py
+++ app.py
@@ -1,23 +1,46 @@
-from flask import Flask, request, send_file
-app = Flask(__name__)
+from typing import Union, Optional
+
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
 from ldm.generate import Generate
 from omegaconf import OmegaConf
 import random
 
 
-@app.route("/")
+class Generation(BaseModel):
+    model: Optional[str] = "stable-diffusion_1-4"
+    prompt: Optional[str] = "An astronaut riding a horse."
+    iterations: Optional[int] = 1
+    steps: Optional[int] = 50
+    seed: Optional[int] = random.randint(1, 99999)
+    cfg_scale: Optional[float] = 7.5
+    seamless: Optional[bool] = False
+    init_img: Optional[str] = None
+    init_mask: Optional[str] = None
+    strength: Optional[float] = 0.75
 
-def index():
+
+app = FastAPI()
+
+"""
+@app.get("/")
+def run_root():
+    return root()
+@app.post("/")
+def root():
     return "hello world"
+"""
+#@app.get("/api")
+#@app.post("/api")
+@app.get("/")
+async def root(generation: Generation = None):
 
-
-@app.route("/api", methods=["GET", "POST"])
-def api():
-
-    args = request.args
+    if not generation:
+        generation = Generation()
 
     # Get models and configs
-    model = args.get("model", default="waifu-diffusion_1-2")
+    model = generation.model
     models = OmegaConf.load("configs/models.yaml")
     width = models[model].width
     height = models[model].height
@@ -25,52 +48,27 @@ def api():
     weights = models[model].weights
 
     arg_dict = {
-        "prompt": args.get("prompt", default="an astronaut riding a horse"),
-        "iterations": args.get("iterations", default=1),
-        "steps": args.get("steps", default=50),
-        "seed": args.get("seed", default=random.randint(1, 99999)),
-        "cfg_scale": args.get("cfg_scale", default=7.5),
+        "prompt": generation.prompt,
+        "iterations": generation.iterations,
+        "steps": generation.steps,
+        "seed": generation.seed,
+        "cfg_scale": generation.cfg_scale,
         "width": width,
         "height": height,
-        "seamless": args.get("seamless", default=False),
-        "init_img": args.get("init_img", default=None), # For img2img
-        "init_mask": args.get("init_mask", default=None), # For inpainting
-        "strength": args.get("strength", default=0.75)
+        "seamless": generation.seamless,
+        "init_img": generation.init_img, # For img2img
+        "init_mask": generation.init_mask, # For inpainting
+        "strength": generation.strength
     }
 
     generation = Generate(weights=weights, config=config)
     output = generation.prompt2png(**arg_dict, outdir="outputs/web_out")
 
-    return send_file(output[0][0], mimetype="image/png")
-
-
-
-
-    """
-    init_params = {
-        "iterations": request_json["iterations"] or 1,
-        "steps": request_json["steps"] or 50,
-        "cfg_scale": request_json["cfg_scale"] or 7.5,
-        "weights": weights,
-        "config": config,
-        "width": width,
-        "height": height,
-        "strength": request_json["strength"] or 0.75,
-        "seamless": request_json["seamless"] or False,
-        "embedding_path": "embeddings/" + request_json["embeddings"] + ".pt" or None,
-    }
-
-    generate_params = {
-        "prompt": request_json["prompt"] or "",
-        "seed": request_json["seed"] or random.randint(1, 99999),
-        "init_image": request_json["init_image"] or None,
-        "mask_image": request_json["mask_image"] or None
-    }
-
-    generation = Generate(**init_params)
-    result = generation.prompt2png(**generate_params)
-    """
+    #return send_file(output[0][0], mimetype="image/png")
+    #return "something"
+    return FileResponse(output[0][0])
 
 
 if __name__ == "__main__":
-    app.run()
+    import uvicorn
+    uvicorn.run(app, host="216.153.51.112", port=8001, log_level="debug")
